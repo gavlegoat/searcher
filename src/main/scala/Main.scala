55ebb34cfe2841c2abf683363b54ac697cf3ec5a
gavlegoat <ganderso@cs.utexas.edu>
2018-10-25 22:21:46 -0500

    Added some initial GUI stuff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
-object Main {
+import java.awt.event.ActionEvent
 
+import javax.swing._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val frame = new JFrame()
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setSize(200, 200)
+    frame.setLocationRelativeTo(null)
+    val button = new JButton("Quit")
+    button.addActionListener((e: ActionEvent) => System.exit(0))
+    frame.add(button)
+    frame.setVisible(true)
+  }
 }
